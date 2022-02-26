18a5d4522f776b26d287a810dda535cc895edfeb
Aidas <aidas004@gmail.com>
2022-02-26 21:55:33 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import RegistrationPage from "./pages/RegistrationPage"
+import LoginPage from "./pages/LoginPage";
+import ToolBar from "./components/ToolBar";
+import AllAuctions from "./pages/AllAuctions";
+import CreateAuction from "./pages/CreteAuction";
+import SinglePost from "./pages/SinglePost";
+import {useState} from "react";
 
 function App() {
+const [user, setUser] = useState()
+    const [timeNow, setTimeNow] = useState()
+    const [countdown, setCountDown] = useState ()
+
+    const now = new Date().getTime();
+    function onclick () {
+        setCountDown(now + 60000)
+        console.log(countdown)
+    }
+
+
+
+    function getTime () {
+        const now = new Date().getTime();
+        const timeNow = new Date(now).toLocaleTimeString("lt-LT");
+        setTimeNow(timeNow)
+    }
+    const a = setInterval(getTime, 1000)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex column">
+        <Router>
+            <ToolBar timeNow={timeNow} user={user}/>
+            <Routes>
+                <Route path="/register" element={<RegistrationPage/>}/>
+                <Route path="/login" element={<LoginPage setUser={setUser}/>}/>
+                <Route path="/create" element={<CreateAuction />}/>
+                <Route path="/" element={<AllAuctions onclick={onclick}  />}/>
+                <Route path="/getSingle/:id" element={<SinglePost timeNow={timeNow} setUser={setUser} user={user} />}/>
+            </Routes>
+        </Router>
+
     </div>
   );
 }
